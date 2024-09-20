d01149d48bcc7ef3c1e24211f38463cfd3fa70b3
Sachin RAWAT <sachinrawatdmc@gmail.com>
2024-09-20 21:33:05 +0530

    Kadane Algorithm

--- Problems/121.bestTimeToBuyAndSellStock.js
+++ Problems/121.bestTimeToBuyAndSellStock.js
@@ -27,22 +27,23 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let max = -Infinity;
-  let i = 1;
-  let count = 0;
+  let buy = prices[0];
+  let profit = 0;
 
-  while (count !== prices.length - 1) {
-    if (i === prices.length) {
-      count++;
-      i = count + 1;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < buy) {
+      buy = prices[i];
+    } else if (prices[i] - buy > profit) {
+      profit = prices[i] - buy;
     }
-
-    let tempMax = prices[i] - prices[count];
-    if (tempMax > max) max = tempMax;
-
-    i++;
   }
-  if (max === -Infinity || max === -1) max = 0;
-  console.log(max);
-  return max;
+
+  return profit;
 };
+
+// Kadane's Algorithm
+// Kadane's Algorithm is a dynamic programming technique used to find the maximum subarray sum in an array of numbers. The algorithm maintains two variables: max_current represents the maximum sum ending at the current position, and max_global represents the maximum subarray sum encountered so far. At each iteration, it updates max_current to include the current element or start a new subarray if the current element is larger than the accumulated sum. The max_global is updated if max_current surpasses its value.
+
+// Complexity
+// Time complexity: O(n), where n is the length of the prices array. The algorithm iterates through the array once.
+// Space complexity: O(1), as only a constant amount of extra space is used.

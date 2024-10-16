4bca37a65a2e04ccb81c559b662f51f1be2d4a52
Sachin RAWAT <sachinrawatdmc@gmail.com>
2024-10-16 19:24:12 +0530

    Two more approaches

--- Problems/9.PalindromeNumber.js
+++ Problems/9.PalindromeNumber.js
@@ -37,3 +37,15 @@ var isPalindrome = function (x) {
   }
   return k === newNum;
 };
+
+var isPalindrome = function (x) {
+  const str = String(x);
+  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+    if (str.charAt(i) !== str.charAt(str.length - i - 1)) return false;
+  }
+  return true;
+};
+
+var isPalindrome = function (x) {
+  return x.toString().split("").reverse().join("") == x;
+};

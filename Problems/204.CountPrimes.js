2256ee1f01aa75d93b2e523583e87c1aa30b0f8e
Sachin RAWAT <sachinrawatdmc@gmail.com>
2024-10-18 19:00:43 +0530

    Sieve of Eratosthenes

--- Problems/204.CountPrimes.js
+++ Problems/204.CountPrimes.js
@@ -21,15 +21,17 @@
  * @param {number} n
  * @return {number}
  */
+
+//Using Sieve of Eratosthenes
 var countPrimes = function (n) {
-  if (n < 2) return 0;
+  if (n <= 2) return 0;
 
   let isPrime = new Array(n).fill(true);
-  isPrime[0] = isPrime[1] = false;
-  for (let i = 0; i < isPrime.length; i++) {
+  isPrime[0] = isPrime[1] = false; //0 and 1 are non-prime so set them as false;
+  for (let i = 2; i < isPrime.length; i++) {
     if (isPrime[i]) {
-      for (let j = i * i; j < n; j += i) isPrime[j] = false;
+      for (let j = i * i; j < n; j += i) isPrime[j] = false; //Factors of non-prime will be prime so mark them as false
     }
   }
-  return isPrime.reduce((acc, curr) => curr + acc, 0);
+  return isPrime.reduce((acc, curr) => curr + acc, 0); //Return the count of true elements in the array
 };

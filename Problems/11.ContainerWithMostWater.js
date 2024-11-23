e901c82472871ade7f7c62bb7be485f1ffea518e
Sachin RAWAT <sachinrawatdmc@gmail.com>
2024-11-23 22:01:33 +0530

    Container with most water - Two Pointers

--- Problems/11.ContainerWithMostWater.js
+++ Problems/11.ContainerWithMostWater.js
@@ -43,3 +43,28 @@ var maxArea = function (height) {
 
   return max === -Infinity ? 0 : max;
 };
+
+//Using Two Pointers
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    let width = right - left;
+    let h = Math.min(height[left], height[right]);
+
+    max = Math.max(max, h * width);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else right--;
+  }
+
+  return max;
+};

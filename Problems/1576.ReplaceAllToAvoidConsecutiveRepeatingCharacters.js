42ef98ddfada8508672b3b42ff222022f9980fa2
Sachin RAWAT <sachinrawatdmc@gmail.com>
2025-02-01 17:41:57 +0530

    1576

---
+++ Problems/1576.ReplaceAllToAvoidConsecutiveRepeatingCharacters.js
@@ -0,0 +1,39 @@
+// Given a string s containing only lowercase English letters and the '?' character, convert all the '?' characters into lowercase letters such that the final string does not contain any consecutive repeating characters. You cannot modify the non '?' characters.
+
+// It is guaranteed that there are no consecutive repeating characters in the given string except for '?'.
+
+// Return the final string after all the conversions (possibly zero) have been made. If there is more than one solution, return any of them. It can be shown that an answer is always possible with the given constraints.
+
+// Example 1:
+
+// Input: s = "?zs"
+// Output: "azs"
+// Explanation: There are 25 solutions for this problem. From "azs" to "yzs", all are valid. Only "z" is an invalid modification as the string will consist of consecutive repeating characters in "zzs".
+// Example 2:
+
+// Input: s = "ubv?w"
+// Output: "ubvaw"
+// Explanation: There are 24 solutions for this problem. Only "v" and "w" are invalid modifications as the strings will consist of consecutive repeating characters in "ubvvw" and "ubvww".
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consist of lowercase English letters and '?'.
+const modifyString = function (s) {
+  let res = [...s];
+
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] !== "?") continue;
+    if (res[i - 1] !== "a" && res[i + 1] !== "a") {
+      res[i] = "a";
+      continue;
+    }
+    if (res[i - 1] !== "b" && res[i + 1] !== "b") {
+      res[i] = "b";
+      continue;
+    }
+    res[i] = "c";
+  }
+
+  return res.join("");
+};
